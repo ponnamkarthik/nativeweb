ba5d1dd736ffb3c6ead24a3bb53609bab3c3f8c2
Karthik Ponnam <ponnamkarthik3@gmail.com>
2018-12-12 09:29:58 +0530

    iOS Fixes

--- ios/Classes/NativewebPlugin.m
+++ ios/Classes/NativewebPlugin.m
@@ -2,20 +2,11 @@
 #import "FlutterWeb.h"
 
 @implementation NativewebPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"nativeweb"
-            binaryMessenger:[registrar messenger]];
-  NativewebPlugin* instance = [[NativewebPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
-}
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterWebFactory* webviewFactory =
+      [[FlutterWebFactory alloc] initWithMessenger:registrar.messenger];
+  [registrar registerViewFactory:webviewFactory withId:@"nativeweb"];
 }
 
 @end
